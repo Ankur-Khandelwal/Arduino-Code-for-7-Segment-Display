c795f17b3a03678b3f7b3ec70a452635593f2632
Ankur Khandelwal <55295915+Ankur-Khandelwal@users.noreply.github.com>
2019-12-10 19:36:25 +0530

    Add files via upload

---
+++ 7Segment.ino
@@ -0,0 +1,53 @@
+int a=6;
+int b=5;
+int c=11;
+int d=10;
+int e=9;
+int f=7;
+int g=8;
+
+void setup() {
+ pinMode(a,OUTPUT); 
+ pinMode(b,OUTPUT); 
+ pinMode(c,OUTPUT); 
+ pinMode(d,OUTPUT); 
+ pinMode(e,OUTPUT); 
+ pinMode(f,OUTPUT); 
+ pinMode(g,OUTPUT); 
+ }
+
+ void loop() {
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(b,HIGH);digitalWrite(c,HIGH); delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(b,HIGH);digitalWrite(g,HIGH);digitalWrite(d,HIGH);digitalWrite(e,HIGH); delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(b,HIGH);digitalWrite(g,HIGH);digitalWrite(c,HIGH);digitalWrite(d,HIGH); delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(f,HIGH);digitalWrite(g,HIGH);digitalWrite(b,HIGH);digitalWrite(c,HIGH);delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(f,HIGH);digitalWrite(g,HIGH);digitalWrite(c,HIGH);digitalWrite(d,HIGH);delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(c,HIGH);digitalWrite(d,HIGH);digitalWrite(e,HIGH);digitalWrite(f,HIGH);digitalWrite(g,HIGH);delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(b,HIGH);digitalWrite(c,HIGH);delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(b,HIGH);digitalWrite(c,HIGH);digitalWrite(d,HIGH);digitalWrite(e,HIGH);digitalWrite(f,HIGH);digitalWrite(g,HIGH);delay(500);
+  digitalWrite(a,LOW); digitalWrite(b,LOW); digitalWrite(c,LOW); digitalWrite(d,LOW); digitalWrite(e,LOW); digitalWrite(f,LOW); digitalWrite(g,LOW);delay(500);
+  digitalWrite(a,HIGH);digitalWrite(b,HIGH);digitalWrite(c,HIGH);digitalWrite(d,HIGH);digitalWrite(f,HIGH);digitalWrite(g,HIGH);delay(500);
+
+
+  
+
+
+
+
+
+
+
+
+
+  
+  
+  
+ }
